5f6dfa71933902c9213da347238cf689d98172ad
Brian Kinney <webdevbrian@gmail.com>
2016-03-24 16:47:12 -0400

    Beginning remapping

--- jquery.simpleWeather.js
+++ jquery.simpleWeather.js
@@ -1,27 +1,21 @@
-/*! simpleWeather v3.1.0 - http://simpleweatherjs.com */
+/*! simpleWeather v3.2.0 - http://simpleweatherjs.com */
 (function($) {
   'use strict';
-
-  function getAltTemp(unit, temp) {
-    if(unit === 'f') {
-      return Math.round((5.0/9.0)*(temp-32.0));
-    } else {
-      return Math.round((9.0/5.0)*temp+32.0);
-    }
-  }
 
   $.extend({
     simpleWeather: function(options){
       options = $.extend({
         location: '',
-        woeid: '',
+        apiKey: '8c841aee3f8442a8',
         unit: 'f',
         success: function(weather){},
         error: function(message){}
       }, options);
 
-      var now = new Date();
-      var weatherUrl = 'https://query.yahooapis.com/v1/public/yql?format=json&rnd=' + now.getFullYear() + now.getMonth() + now.getDay() + now.getHours() + '&diagnostics=true&callback=?&q=';
+      //http://api.wunderground.com/api/8c841aee3f8442a8/conditions/q/CA/San_Francisco.json
+      // need to format location.
+      //
+      var weatherUrl = 'http://api.wunderground.com/api/'+options.apiKey+'/conditions/q/'+options.location+'.json'; //add parameters for location
 
       if(options.location !== '') {
         /* If latitude/longitude coordinates, need to format a little different. */
@@ -32,45 +26,41 @@
           location = options.location;
         }
 
-        weatherUrl += 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '") and u="' + options.unit + '"';
-      } else if(options.woeid !== '') {
-        weatherUrl += 'select * from weather.forecast where woeid=' + options.woeid + ' and u="' + options.unit + '"';
-      } else {
-        options.error('Could not retrieve weather due to an invalid location.');
-        return false;
-      }
-
       $.getJSON(
         encodeURI(weatherUrl),
         function(data) {
-          if(data !== null && data.query !== null && data.query.results !== null && data.query.results.channel.description !== 'Yahoo! Weather Error') {
-            var result = data.query.results.channel,
+          if(data !== null && data.query !== null && data.query.results !== null && data.query.results.channel.description !== 'Weather Underground Error') {
+            var result = data,
                 weather = {},
                 forecast,
                 compass = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N'],
                 image404 = 'https://s.yimg.com/os/mit/media/m/weather/images/icons/l/44d-100567.png';
 
-            weather.title = result.item.title;
-            weather.temp = result.item.condition.temp;
-            weather.code = result.item.condition.code;
-            weather.todayCode = result.item.forecast[0].code;
-            weather.currently = result.item.condition.text;
-            weather.high = result.item.forecast[0].high;
-            weather.low = result.item.forecast[0].low;
-            weather.text = result.item.forecast[0].text;
-            weather.humidity = result.atmosphere.humidity;
-            weather.pressure = result.atmosphere.pressure;
-            weather.rising = result.atmosphere.rising;
-            weather.visibility = result.atmosphere.visibility;
-            weather.sunrise = result.astronomy.sunrise;
-            weather.sunset = result.astronomy.sunset;
-            weather.description = result.item.description;
-            weather.city = result.location.city;
-            weather.country = result.location.country;
-            weather.region = result.location.region;
-            weather.updated = result.item.pubDate;
-            weather.link = result.item.link;
-            weather.units = {temp: result.units.temperature, distance: result.units.distance, pressure: result.units.pressure, speed: result.units.speed};
+            weather.title = 'Conditions for '+result.full+' at '+result.local_time_rfc822;
+            weather.temp = result.temp_+options.unit; // result.temp_f or result.temp_c
+            //weather.code = result.item.condition.code;
+            //weather.todayCode = result.item.forecast[0].code;
+            weather.temperature_string = result.temperature_string;
+            weather.currently = result.weather;
+            //weather.high = result.item.forecast[0].high;
+            //weather.low = result.item.forecast[0].low;
+            //weather.text = result.item.forecast[0].text;
+            weather.humidity = result.relative_humidity;
+            weather.pressure = result.pressure_in;
+            weather.pressureMB = result.pressure_mb; /* NEW */
+            weather.rising = result.pressure_trend; /* NEW - returns + or - */
+            weather.visibility = result.visibility_mi; /* CHANGED - response is either MI or KM (visibility_mi or visibility_km) */
+            //weather.sunrise = result.astronomy.sunrise;
+            //weather.sunset = result.astronomy.sunset;
+            weather.description = result.item.description; // not in docs to say what this returned?
+            weather.city = result.city;
+            weather.country = result.country;
+            //weather.region = result.location.region;
+            weather.observation_time = result.item.pubDate;
+            weather.link = result.forecast_url;
+
+            weather.units = {temp: options.unit, distance: 'distance is deprecated', pressure: result.pressure_in, speed: result.units.speed};
+
             weather.wind = {chill: result.wind.chill, direction: compass[Math.round(result.wind.direction / 22.5)], speed: result.wind.speed};
 
             if(result.item.condition.temp < 80 && result.atmosphere.humidity < 40) {
@@ -83,8 +73,8 @@
               weather.thumbnail = image404;
               weather.image = image404;
             } else {
-              weather.thumbnail = 'https://s.yimg.com/zz/combo?a/i/us/nws/weather/gr/' + result.item.condition.code + 'ds.png';
-              weather.image = 'https://s.yimg.com/zz/combo?a/i/us/nws/weather/gr/' + result.item.condition.code + 'd.png';
+              weather.thumbnail = 'http://icons-ak.wxug.com/i/c/k/' + result.item.condition.code + '.gif'; // http://icons-ak.wxug.com/i/c/k/partlycloudy.gif
+              weather.image = 'http://icons-ak.wxug.com/i/c/k/' + result.item.condition.code + '.gif'; // http://icons-ak.wxug.com/i/c/k/partlycloudy.gif
             }
 
             weather.alt = {temp: getAltTemp(options.unit, result.item.condition.temp), high: getAltTemp(options.unit, result.item.forecast[0].high), low: getAltTemp(options.unit, result.item.forecast[0].low)};
